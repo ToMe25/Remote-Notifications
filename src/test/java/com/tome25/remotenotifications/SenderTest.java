92c11d642ddaaa34602a390b4fdc4ef783a9bf25
ToMe25 <38815969+ToMe25@users.noreply.github.com>
2020-05-30 09:57:18 +0200

    How did the wrong junit get imported?

--- src/test/java/com/tome25/remotenotifications/SenderTest.java
+++ src/test/java/com/tome25/remotenotifications/SenderTest.java
@@ -1,6 +1,6 @@
 package com.tome25.remotenotifications;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.Assert.assertEquals;
 
 import java.io.IOException;
 import java.io.InputStream;
